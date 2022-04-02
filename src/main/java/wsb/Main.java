14e26aabedb00f7c853c7dbe616ac5ebbbbf6a3a
giuseppetumminello <giuseppe_1995@live.it>
2022-04-02 11:29:51 +0200

    Basic Structure for Task 2 - update

--- src/main/java/wsb/Main.java
+++ src/main/java/wsb/Main.java
@@ -8,7 +8,7 @@ public class Main {
         CurrencyAPIConnector connector = new CurrencyAPIConnector();
         System.out.println(connector.getLatestData());
 
-        getExchangeHistorical("2022-02-02", 2000.0);
+        getExchangeHistorical("2022-03-07", 2000.0);
 
 
 
@@ -19,12 +19,14 @@ public class Main {
         double historicalValue = currencyAPIConnector.getHistoricalData("USD", purchaseDate).getJSONObject("data").getJSONObject("PLN").getDouble("value");
         double latestValue = currencyAPIConnector.getLatestData().getJSONObject("data").getJSONObject("PLN").getDouble("value");
         if (historicalValue < latestValue){
-            System.out.println("This is a goo moment to sell USD and buy PLN, 1.0 USD is equal to: " + latestValue);
+            System.out.println("This is a good moment to sell USD and buy PLN, 1.0 USD is equal to: " + latestValue);
             System.out.println("You will earn: " + ((latestValue*amount) - (historicalValue*amount)) + "PLN");
         } else{
             System.out.println("This is not  a good moment to sell USD and buy PLN, 1.0 USD is equal to: " + historicalValue);
-            System.out.println("You will earn: " + ((historicalValue*amount) - (latestValue*amount)) +"PLN");
+            System.out.println("You will loose: " + ((historicalValue*amount) - (latestValue*amount)) +"PLN");
         }
+
+
 
     }
 
